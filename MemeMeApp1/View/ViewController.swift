894bf1778b811f1e61ead37b4fb3ffeacf1d6aaa
joel stevick <joelstevick@outlook.com>
2022-04-21 18:51:57 -0400

    reset after keyboard hides

--- MemeMeApp1/View/ViewController.swift
+++ MemeMeApp1/View/ViewController.swift
@@ -47,13 +47,18 @@ class ViewController: UIViewController,  UIImagePickerControllerDelegate, UINavi
     @objc func keyboardWillShow(_ notification: Notification) {
         view.frame.origin.y = -1 * getKeyboardHeight(notification)
     }
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
     
     func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     func unsubscribeToKeyboardNotifications () {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil )
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil )
     }
     
     func getKeyboardHeight(_ notification: Notification) -> CGFloat {
