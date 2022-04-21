e7f64dbfd8b08ddb72de711e0829d928fe208b2a
joel stevick <joelstevick@outlook.com>
2022-04-21 16:52:12 -0400

    Initial Commit

---
+++ MemeMeApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMeApp1
+//
+//  Created by Joel Stevick on 4/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MemeMeApp1UITests/MemeMeApp1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MemeMeApp1UITestsLaunchTests.swift
+//  MemeMeApp1UITests
+//
+//  Created by Joel Stevick on 4/21/22.
+//
+
+import XCTest
+
+class MemeMeApp1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
