a028f09d3cbac3c7abcd1c7211835b9c8bd71c63
joel stevick <joelstevick@outlook.com>
2022-04-21 17:25:09 -0400

    placed the top and bottom

--- MemeMeApp1/ViewController.swift
+++ MemeMeApp1/ViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 class ViewController: UIViewController {
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.blue,
-        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.foregroundColor: UIColor.blue,
         NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
         NSAttributedString.Key.strokeWidth:  0
     ]
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        topText.defaultTextAttributes = memeTextAttributes
+        bottomText.defaultTextAttributes = memeTextAttributes
+        
     }
 
 
